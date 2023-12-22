cea3a5127fc6a245a4887f6d502cc7f9c312e984
Samia3 <samiaamrouche94@gmail.com>
2023-12-22 21:05:35 +0100

    afficher l'alphabet

--- exo00.js
+++ exo00.js
@@ -1,1 +1,11 @@
-console.log("hello");
+// Variable pour stocker l'alphabet
+let alphabet = '';
+
+// Boucle pour chaque code ASCII de 'a' à 'z'
+for (let codeAscii = 97; codeAscii <= 122; codeAscii++) {
+    // Convertit le code ASCII en lettre et concatène à la variable alphabet
+    alphabet += String.fromCharCode(codeAscii);
+}
+
+// Affiche l'alphabet sur une seule ligne
+console.log(alphabet);
