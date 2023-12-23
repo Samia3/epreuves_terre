65bc90fe76fe7c80c6887ed4765e64a83322ba56
Samia3 <samiaamrouche94@gmail.com>
2023-12-23 03:53:33 +0100

    exercice afficher nom du programme

--- exo01.js
+++ exo01.js
@@ -1,3 +1,10 @@
+// process.argv est un tableau (array) en Node.js qui contient les arguments de la ligne de commande. Plus précisément, il contient les éléments suivants :
+
+// process.argv[0]: Chemin vers l'exécutable Node.js.
+// process.argv[1]: Chemin vers le script Node.js en cours d'exécution.
+// process.argv[2] et au-delà: Les arguments fournis par l'utilisateur lors de l'exécution du script.
+
+
 // Récupère le nom du fichier en cours d'exécution à partir de process.argv
 const path = process.argv[1];
 
