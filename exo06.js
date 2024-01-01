15bf28089264de83b436ae88abe286c3bfc6fae1
Samia3 <samiaamrouche94@gmail.com>
2024-01-01 20:30:23 +0100

    inverser une chaine de caractère

---
+++ exo06.js
@@ -0,0 +1,26 @@
+function argument(){
+    // Récupère les arguments à partir du deuxième index
+    const arguments = process.argv.slice(2);
+
+    //expression régulière pour vérifier si l'argument est une chaine de caractère
+    const saisie = /^[a-zA-Z\s]+$/;
+
+    if(arguments.length != 1 || !saisie.test(arguments[0])){
+        console.log("veuillez mettre un seul argument (juste des lettres entre guillemets).");
+    }else{
+        inverser();
+    }
+}
+
+function inverser(){
+    let lettre = process.argv[2];
+    let result = '';
+
+    for(let i= lettre.length -1; i >= 0 ; i--){
+        result += lettre[i];
+    }
+
+    console.log(result);
+}
+
+argument();
